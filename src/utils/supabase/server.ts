00afe3493f6b8464566360fc4d75664092172407
Dai <let__it__be@hb.tp1.jp>
2025-01-01 13:35:04 +0900

    環境変数設定

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -2,10 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 // 環境変数を使用してURLとキーを管理
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabaseUrl = 'https://uglqyxwvqhygrwqgexhy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnbHF5eHd2cWh5Z3J3cWdleGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU2MTE4OTAsImV4cCI6MjA1MTE4Nzg5MH0.nwj5e2NszxHC5c3XTqkaDaWG8Ej0h1u3rOOj9Q5tG6U';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// const supabaseUrl = 'https://uglqyxwvqhygrwqgexhy.supabase.co';
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnbHF5eHd2cWh5Z3J3cWdleGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU2MTE4OTAsImV4cCI6MjA1MTE4Nzg5MH0.nwj5e2NszxHC5c3XTqkaDaWG8Ej0h1u3rOOj9Q5tG6U';
 
 // Supabaseクライアントの作成
 export const supabase = createClient(supabaseUrl, supabaseKey);
